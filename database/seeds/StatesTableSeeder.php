aed283f67c681cbc24b2e07722c0acd85c03a4ba
Matthew Allan <matt@mattallan.me>
2020-01-10 12:01:10 -0500

    init

---
+++ database/seeds/StatesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('states')->insert([
+            'id' => 'NC',
+        ]);
+
+        DB::table('states')->insert([
+            'id' => 'FL',
+        ]);
+    }
+}
